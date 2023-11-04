fbbc7a8d87764228d2200f4807f271cc94418783
Yazan Salameh <yazan@serenbit.ps>
2023-11-04 18:48:18 +0200

    Build home view

--- src/Api/Controllers/HomeController.cs
+++ src/Api/Controllers/HomeController.cs
@@ -1,3 +1,4 @@
+using CloudDrive.Services.Files;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
@@ -6,8 +7,23 @@ namespace CloudDrive.Api.Controllers
 	[Route("/")]
 	public class HomeController : Controller
 	{
+		private readonly IFilesService _service;
+
+		public HomeController(IFilesService service)
+		{
+			_service = service;
+		}
+
 		[HttpGet]
-		public IActionResult Get()
+		public async Task<IActionResult> Get()
+		{
+			var files = await _service.Get();
+
+			return View(files);
+		}
+
+		[HttpGet("/Terms")]
+		public IActionResult Terms()
 		{
 			return View();
 		}
