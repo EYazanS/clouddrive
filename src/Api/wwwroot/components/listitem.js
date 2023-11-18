1ad9e05035e87f6eaafc46393a26c6182649e23b
Dima Taqatqa <88555807+DimaTaqatqa@users.noreply.github.com>
2023-11-18 17:54:18 +0200

    finish the list web component in the index page

--- src/Api/wwwroot/components/listitem.js
+++ src/Api/wwwroot/components/listitem.js
@@ -1,165 +1,114 @@
 console.log("list item");
 
-class MyListItem extends HTMLElement {
+
+class ListItem extends HTMLElement {
+
+	listClass = "my-8 list-decimal ml-10 text-l font-bold font-mono dark:text-primary-100";
+	divClass = "inline-flex ml-3";
+	aClass = "text-primary-600 hover:underline dark:text-primary-400";
+
+	div;
+	list;
+	aEdit;
+	aDelete;
+
+	area = "Notebooks";
+	deletePage = "/Delete";
+	editPage = "/Form";
+
+
 	constructor() {
 		super();
+		this.createList();
+		this.createDiv();
+		this.createDelete();
+		this.createEdit();
 
-		// Create a shadow root
-		this.attachShadow({ mode: 'open' });
+		this.appendChild(this.list);
+		this.list.appendChild(this.div);
+		this.div.appendChild(this.aDelete);
+		this.div.appendChild(this.aEdit);
 
-		// Clone the template content and attach it to the shadow root
-		this.shadowRoot.innerHTML = `
-      <style>
-        :host {
-          display: block;
-          margin-top: 8px;
-          list-style-type: decimal;
-          margin-left: 10px;
-          font-size: 1rem;
-          font-weight: bold;
-          font-family: 'Monospace', monospace; /* Adjust font-family as needed */
-          color: #000; /* Adjust text color */
-        }
+		this.aEdit.addEventListener('click', this.handleEditClick.bind(this));
+		this.aDelete.addEventListener('click', this.handleDeleteClick.bind(this));
 
-        .inline-flex {
-          display: inline-flex;
-          margin-left: 3px;
-        }
-
-        a {
-          margin-right: 3px;
-          color: #0066cc; /* Adjust link color */
-          text-decoration: none;
-        }
-
-        a:hover {
-          text-decoration: underline;
-        }
-
-        .dark-text {
-          color: #A0AEC0; /* Adjust dark mode text color */
-        }
-      </style>
-      <li class="my-8 list-decimal ml-10 text-l font-bold font-mono dark-text">
-        ${this.getAttribute('name')}
-        <div class="inline-flex ml-3">
-          <a class="delete-link">Delete</a>
-          <a  class="edit-link">Edit</a>
-        </div>
-      </li>
-    `;
-
-		// Set up event listeners or any additional logic here
-		this.shadowRoot.querySelector('.delete-link').addEventListener('click', this.handleDelete.bind(this));
-		this.shadowRoot.querySelector('.edit-link').addEventListener('click', this.handleEdit.bind(this));
+	}
+	static get observedAttributes() {
+		return ["name", "item-id"];
 	}
 
-	// Define methods or event handlers here
-	handleDelete() {
-		// Implement delete logic
-		console.log('Delete clicked');
-	}
-
-	handleEdit() {
-		// Implement edit logic
-		console.log('Edit clicked');
-		const itemId = this.getAttribute('item-id');
-		if (itemId) {
-			// Create an anchor element dynamically
-			const editLink = document.createElement('a');
-
-			// Set the necessary attributes
-			editLink.setAttribute('asp-area', 'Notebooks');
-			editLink.setAttribute('asp-page', '/Form');
-			editLink.setAttribute('class', 'text-primary-600 hover:underline dark:text-primary-400');
-			editLink.setAttribute('asp-page-handler', 'Update');
-			editLink.setAttribute('asp-route-id', itemId);
-
-			// Append the anchor element to the body
-			document.body.appendChild(editLink);
-
-			// Simulate a click on the anchor element to navigate to the specified page
-			editLink.click();
-
-			document.body.removeChild(editLink);
-		}
-	}
-
-	// Called when the element is connected to the DOM
 	connectedCallback() {
-		console.log('Element added to the DOM');
+
 	}
 
-	// Called when the element is disconnected from the DOM
-	disconnectedCallback() {
-		console.log('Element removed from the DOM');
+	createList() {
+		this.list = document.createElement("li");
+		const itemName = this.getAttribute("name");
+
+		if (itemName) {
+			this.list.textContent = itemName;
+		} else {
+			// Provide a default text or handle the case when 'name' attribute is not set
+			this.list.textContent = "Default Item Name";
+		}
+
+		this.listClass
+			.split(" ")
+			.forEach((classs) => this.list.classList.add(classs));
 	}
+
+	createDiv() {
+		this.div = document.createElement("div");
+		this.divClass
+			.split(" ")
+			.forEach((classs) => this.div.classList.add(classs));
+	}
+	createDelete() {
+		this.aDelete = document.createElement("a");
+		this.aDelete.textContent = "Delete"
+		this.aDelete.setAttribute('asp-area', this.area);
+		this.aDelete.setAttribute('asp-route-id', this.getAttribute('item-id'));
+		this.aDelete.setAttribute('asp-page', this.deletePage);
+
+		this.aDelete.classList.add("mr-3");
+
+		this.aClass
+			.split(" ")
+			.forEach((classs) => this.aDelete.classList.add(classs));
+	}
+
+	createEdit() {
+		this.aEdit = document.createElement("a");
+		this.aEdit.textContent = "Edit"
+		this.aEdit.setAttribute('asp-area', this.area);
+		this.aEdit.setAttribute("asp-route-id", this.getAttribute('item-id'));
+		this.aEdit.setAttribute('asp-page', this.editPage);
+		const pageHandler = this.getAttribute('asp-page-handler');
+
+		if (pageHandler) {
+			this.aEdit.setAttribute('asp-page-handler', pageHandler);
+		}
+
+		this.aClass
+			.split(" ")
+			.forEach((classs) => this.aEdit.classList.add(classs));
+	}
+
+	handleEditClick() {
+		const itemId = this.getAttribute('item-id');
+		const handler = this.getAttribute('asp-page-handler')
+		const editPageUrl = `${this.area}/${handler}/${itemId}`;
+
+		// Navigate to the Edit page
+		window.location.href = editPageUrl;
+	}
+	handleDeleteClick() {
+		const itemId = this.getAttribute('item-id');
+		const editPageUrl = `Notebooks/Delete/${itemId}`;
+
+		// Navigate to the Edit page
+		window.location.href = editPageUrl;
+	}
+
 }
-
-// Define the custom element
-customElements.define('my-list-item', MyListItem);
-
-// class ListItem extends HTMLElement {
-
-// 	listClass = "my-8 list-decimal ml-10 text-l font-bold font-mono dark:text-primary-100";
-// 	divClass = "inline-flex ml-3";
-// 	aClass = "text-primary-600 hover:underline dark:text-primary-400";
-
-// 	div;
-// 	list;
-// 	a;
-
-// 	area = "Notebooks";
-// 	deletePage = "/Delete";
-// 	editPage = "/Form";
-
-
-// 	constructor() {
-// 		super();
-// 		this.createList();
-// 		this.createDiv();
-// 		this.createDelete();
-// 		this.createDiv();
-
-// 		this.appendChild(this.list);
-// 		this.appendChild(this.div);
-// 		this.appendChild(this.a);
-// 		this.appendChild(this.a);
-
-// 	}
-
-// 	connectedCallback() {
-// 	}
-// 	createList() {
-// 		this.list = document.createElement("li");
-// 		this.listClass
-// 			.split(" ")
-// 			.forEach((classs) => this.label.classList.add(classs));
-// 	}
-
-// 	createDiv() {
-// 		this.div = document.createElement("div");
-// 		this.divClass
-// 			.split(" ")
-// 			.forEach((classs) => this.label.classList.add(classs));
-// 	}
-// 	createDelete() {
-// 		this.a = document.createElement("a");
-// 		this.a.area=area;
-// 		this.a.classList.add("mr-3");
-// 		this.aClass
-// 			.split(" ")
-// 			.forEach((classs) => this.label.classList.add(classs));
-// 	}
-// 	createEdit() {
-// 		this.a = document.createElement("a");
-// 		this.a.area=area;
-// 		this.a.
-// 		this.aClass
-// 			.split(" ")
-// 			.forEach((classs) => this.label.classList.add(classs));
-// 	}
-
-
-// }
-// customElements.define("list-item", ListItem);
+customElements.define("list-item", ListItem);
