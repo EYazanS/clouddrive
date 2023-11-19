c19e0e703fcbacaf02b4726e6d808ba425aa4f4e
Yazan Salameh <yazan@serenbit.ps>
2023-11-19 19:00:23 +0200

    add identity project

--- src/Api/Areas/Notebooks/Pages/Index.cshtml.cs
+++ src/Api/Areas/Notebooks/Pages/Index.cshtml.cs
@@ -1,30 +1,32 @@
 using CloudDrive.Services.Notebooks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace CloudDrive.Api.Areas.Notebooks.Pages
 {
-    public class IndexPage : PageModel
-    {
-        private readonly INotebooksService _service;
+	[Authorize]
+	public class IndexPage : PageModel
+	{
+		private readonly INotebooksService _service;
 
-        [BindProperty]
-        public List<NotebookDto> Notebooks { get; set; }
+		[BindProperty]
+		public List<NotebookDto> Notebooks { get; set; }
 
-        [BindProperty(SupportsGet = true)]
-        public int Id { get; set; }
+		[BindProperty(SupportsGet = true)]
+		public int Id { get; set; }
 
-        public IndexPage(INotebooksService service)
-        {
-            _service = service;
-        }
+		public IndexPage(INotebooksService service)
+		{
+			_service = service;
+		}
 
-        public async Task OnGet()
-        {
-            ViewData["PageTitle"] = "Index page";
+		public async Task OnGet()
+		{
+			ViewData["PageTitle"] = "Index page";
 
-            Notebooks = await _service.Get();
-        }
-    }
+			Notebooks = await _service.Get();
+		}
+	}
 }
--- src/Api/Program.cs
+++ src/Api/Program.cs
@@ -15,16 +15,11 @@ using CloudDrive.Services.UserPasswords;
 
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Localization;
-using Microsoft.AspNetCore.Localization.Routing;
 using Microsoft.AspNetCore.Mvc.Razor;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Localization;
 
-using OpenIddict.Validation.AspNetCore;
-
 using Quartz;
-
-using static OpenIddict.Abstractions.OpenIddictConstants;
 
 [assembly: RootNamespace("CloudDrive")]
 
@@ -33,8 +28,6 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<AppDbContext>(options =>
 {
 	options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
-
-	options.UseOpenIddict();
 });
 
 builder
@@ -56,9 +49,31 @@ builder.Services.AddSingleton(new FileConfigurations()
 
 builder.Services.AddSingleton<BackgroundWorkService>();
 
+// Add Authentication
 builder
 	.Services
-	.AddAuthentication(OpenIddictValidationAspNetCoreDefaults.AuthenticationScheme);
+	.AddIdentity<AppUser, IdentityRole>(options =>
+	{
+		options.User.RequireUniqueEmail = true;
+		options.SignIn.RequireConfirmedAccount = true;
+		options.SignIn.RequireConfirmedPhoneNumber = false;
+		options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+
+		options.Password.RequireNonAlphanumeric = false;
+		options.Password.RequireUppercase = false;
+		options.Password.RequireDigit = false;
+		options.Password.RequireLowercase = false;
+		options.Password.RequiredUniqueChars = 0;
+	})
+	.AddDefaultUI()
+	.AddDefaultTokenProviders()
+	.AddEntityFrameworkStores<AppDbContext>();
+
+builder.Services.AddAuthentication();
+
+builder.Services.AddAuthorization();
+
+// Add Localization
 
 builder
 	.Services
@@ -98,93 +113,6 @@ builder
 		};
 	});
 
-
-// Register the Identity services.
-builder
-	.Services
-	.AddIdentity<AppUser, IdentityRole>()
-	.AddEntityFrameworkStores<AppDbContext>()
-	.AddDefaultTokenProviders();
-
-// OpenIddict offers native integration with Quartz.NET to perform scheduled tasks
-// (like pruning orphaned authorizations/tokens from the database) at regular intervals.
-builder
-	.Services
-	.AddQuartz(options =>
-	{
-		options.UseMicrosoftDependencyInjectionJobFactory();
-		options.UseSimpleTypeLoader();
-		options.UseInMemoryStore();
-	});
-
-builder
-	.Services
-	.AddOpenIddict()
-
-	// Register the OpenIddict core components.
-	.AddCore(options =>
-	{
-		// Configure OpenIddict to use the Entity Framework Core stores and models.
-		// Note: call ReplaceDefaultEntities() to replace the default OpenIddict entities.
-		options
-			.UseEntityFrameworkCore()
-			.UseDbContext<AppDbContext>();
-
-		// Enable Quartz.NET integration.
-		options.UseQuartz();
-	})
-
-	// Register the OpenIddict server components.
-	.AddServer(options =>
-	{
-		// Enable the authorization, logout, token and userinfo endpoints.
-		options
-			.SetAuthorizationEndpointUris("/connect/authorize")
-			.SetLogoutEndpointUris("/connect/logout")
-			.SetTokenEndpointUris("/connect/token")
-			.SetUserinfoEndpointUris("/connect/userinfo");
-
-		// Mark the "email", "profile" and "roles" scopes as supported scopes.
-		options.RegisterScopes(Scopes.Email, Scopes.Profile, Scopes.Roles);
-
-		options
-			.AllowPasswordFlow()
-			.AllowAuthorizationCodeFlow()
-			.AllowRefreshTokenFlow();
-
-		options
-			.AddDevelopmentEncryptionCertificate()
-			.AddDevelopmentSigningCertificate();
-
-		// Register the ASP.NET Core host and configure the ASP.NET Core-specific options.
-		options
-			.UseAspNetCore()
-			.EnableAuthorizationEndpointPassthrough()
-			.EnableLogoutEndpointPassthrough()
-			.EnableStatusCodePagesIntegration()
-			.EnableTokenEndpointPassthrough();
-	})
-
-	// Register the OpenIddict validation components.
-	.AddValidation(options =>
-	{
-		// Import the configuration from the local OpenIddict server instance.
-		options.UseLocalServer();
-
-		// Register the ASP.NET Core host.
-		options.UseAspNetCore();
-
-		options.UseDataProtection();
-
-		// For applications that need immediate access token or authorization
-		// revocation, the database entry of the received tokens and their
-		// associated authorizations can be validated for each API call.
-		// Enabling these options may have a negative impact on performance.
-		//
-		// options.EnableAuthorizationEntryValidation();
-		// options.EnableTokenEntryValidation();
-	});
-
 // Register the worker responsible of seeding the database with the sample clients.
 // Note: in a real world application, this step should be part of a setup script.
 
@@ -192,8 +120,6 @@ builder
 builder.Services.AddRazorPages();
 
 builder.Services.AddControllers();
-
-builder.Services.AddHostedService<InitWorker>();
 
 builder.Services.AddHostedService<TimerWorker>();
 
