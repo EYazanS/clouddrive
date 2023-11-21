618d743e6ea047a1efe0e06909817177543834b4
Salem Mufarreh <salemmuf9@gmail.com>
2023-11-21 22:49:48 +0200

    added user credit card component

--- src/Api/wwwroot/components/UserCreditCards.js
+++ src/Api/wwwroot/components/UserCreditCards.js
@@ -1,25 +1,52 @@
 ﻿
 class UserCreditCards extends HTMLElement
 {
-    table;
-    row;
+ 
     constructor() {
         super();
-        this.createRow();
-    }
-    createRow() {
 
     }
-    set card(cardData) {
-        this.updateRowContent(cardData);
-    }
-    updateRowContent(cardData) {
-        // Update cell content based on cardData
-        this.cells[0].textContent = cardData.Id;
-        this.cells[1].textContent = cardData.HolderName;
-        this.cells[2].textContent = cardData.CreditCardNumber;
-        this.cells[3].textContent = `${cardData.ExpireMonth} / ${cardData.ExpireYear}`;
-        // Add action links/buttons update if needed
+    connectedCallback() {
+        const itemId = this.getAttribute('itemid');
+        const cardAttribute = this.getAttribute('card');
+        console.log('cardAttribute:', cardAttribute);
+        const card = JSON.parse(this.getAttribute('card'));
+        const row = document.createElement('tr');
+        row.classList.add("bg-white", "border-b", "dark:bg-gray-800", "dark:border-gray-700", "hover:bg-gray-50", "dark:hover:bg-gray-600");
+
+        const idCell = document.createElement('td');
+        idCell.textContent = card.id;
+        row.appendChild(idCell);
+        const holderNameCell = document.createElement('td');
+        holderNameCell.textContent = card.holderName;
+        row.appendChild(holderNameCell);
+
+        const cardNumberCell = document.createElement('td');
+        cardNumberCell.textContent = card.creditCardNumber;
+        row.appendChild(cardNumberCell);
+
+        const expireDateCell = document.createElement('td');
+        expireDateCell.textContent = `${card.expireMonth} / ${card.expireYear}`;
+        row.appendChild(expireDateCell);
+
+        const actionCell = document.createElement('td');
+        const editLink = document.createElement('a');
+        editLink.href = `/userCredit/Update/${itemId}`;
+        editLink.textContent = 'Edit';
+
+        const deleteLink = document.createElement('a');
+        deleteLink.href = `/userCredit/Delete/${itemId}`;
+        deleteLink.textContent = 'Delete';
+
+        actionCell.appendChild(editLink);
+        actionCell.appendChild(document.createTextNode(' | '));
+        actionCell.appendChild(deleteLink);
+
+        row.appendChild(actionCell);
+
+        // Append the row to the table body
+        const tableBody = document.querySelector('table tbody');
+        tableBody.appendChild(row);
     }
 
     
